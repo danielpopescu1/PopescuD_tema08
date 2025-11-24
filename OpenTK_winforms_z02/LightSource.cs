d5325b6747e6030160d5ea7478be5b99201013cf
danielpopescu1 <daniel.popescu1@student.usv.ro>
2025-11-24 10:23:21 -0800

    Add files via upload

---
+++ OpenTK_winforms_z02/LightSource.cs
@@ -0,0 +1,36 @@
+﻿using OpenTK.Graphics.OpenGL;
+
+namespace OpenTK_winforms_z02
+{
+    public class LightSource
+    {
+        public bool Enabled { get; set; }
+        public int LightId { get; set; }
+
+        public float[] Ambient = new float[4];
+        public float[] Diffuse = new float[4];
+        public float[] Specular = new float[4];
+        public float[] Position = new float[4];
+
+        public LightSource(int id)
+        {
+            LightId = id;
+            Enabled = false;
+        }
+
+        public void Apply()
+        {
+            if (!Enabled)
+            {
+                GL.Disable((EnableCap)LightId);
+                return;
+            }
+
+            GL.Light((LightName)LightId, LightParameter.Ambient, Ambient);
+            GL.Light((LightName)LightId, LightParameter.Diffuse, Diffuse);
+            GL.Light((LightName)LightId, LightParameter.Specular, Specular);
+            GL.Light((LightName)LightId, LightParameter.Position, Position);
+            GL.Enable((EnableCap)LightId);
+        }
+    }
+}
